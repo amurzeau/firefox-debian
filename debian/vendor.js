99fcf40b467d99283d2f73e08757bd80022ce581
Mike Hommey <mh@glandium.org>
2018-05-10 09:46:09 +0900

    Use the new syntax for locked and sticky prefs

--- debian/vendor.js
+++ debian/vendor.js
@@ -1,6 +1,6 @@
 pref("distribution.searchplugins.defaultLocale", "en-US");
 // Forbid application updates
-lockPref("app.update.enabled", false);
+pref("app.update.enabled", false, locked);
 
 pref("devtools.webide.autoinstallADBHelper", false);
 pref("devtools.webide.autoinstallFxdtAdapters", false);
